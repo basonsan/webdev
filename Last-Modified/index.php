be25a54ee7c1d77633529488b4bbfea0ab3e4b4c
bason <basonsan@yandex.ru>
2021-12-03 18:26:36 +0400

    add Last Modified
    Скрипт для определения последнего изменения файла

---
+++ Last-Modified/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- Отправляем HTTP заголовок Last-Modified -->
+    <?php
+        $LastModified_unix = 1294844676; // время последнего изменения страницы
+        $LastModified = gmdate("D, d M Y H:i:s \G\M\T", $LastModified_unix);
+        $IfModifiedSince = false;
+        if (isset($_ENV['HTTP_IF_MODIFIED_SINCE']))
+            $IfModifiedSince = strtotime(substr($_ENV['HTTP_IF_MODIFIED_SINCE'], 5)); 
+        if (isset($_SERVER['HTTP_IF_MODIFIED_SINCE']))
+            $IfModifiedSince = strtotime(substr($_SERVER['HTTP_IF_MODIFIED_SINCE'], 5));
+        if ($IfModifiedSince && $IfModifiedSince >= $LastModified_unix) {
+            header($_SERVER['SERVER_PROTOCOL'] . ' 304 Not Modified');
+            exit;
+        }
+        header('Last-Modified: '. $LastModified);
+    ?>
+</head>
+<body>
+    
+</body>
+</html>
